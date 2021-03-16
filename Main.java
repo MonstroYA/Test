ad683854f8818bcbc93c7cc5ffffbbfc023e0d2d
MonstroYA <56930704+MonstroYA@users.noreply.github.com>
2021-03-16 21:30:51 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String str1 = "xyz";
+        String str2 = "xy";
+        String str3 = "z";
+        String str4 = str1.repeat(3) + " " + str2.repeat(2) + " " + str3.repeat(1);
+
+
+
+        System.out.println(str4 );
+
+    }
+}
